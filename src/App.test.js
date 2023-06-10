bd2c5dcdcc4420faca50fd83f1a97e18439fbff4
Kevin Pérez <perez.kevin.1cv.2017@gmail.com>
2023-06-10 11:40:12 -0600

    Primera version

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './js/Principal/App';
 
 test('renders learn react link', () => {
   render(<App />);
